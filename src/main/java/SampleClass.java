dbaafbf6e0748582c4e2cf1f184f5cafb294f101
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-08 16:37:14 +0000

    Overridden .equals() and .toString() methods

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -62,10 +62,15 @@ public class SampleClass {
     public int a;
     public boolean b;
 
-    //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other){
+        if (this.a == other.a && this.b == other.b) {
+            return true;
+        }
+        return false;
+    }
 
-
-    //    implement a custom .toString(){} method here.
-
+    public String toString(){
+        return "a: " + this.a + "/nb: " + this.b + "/n";
+    }
     
 }
